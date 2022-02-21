3245834d518bcda6205b31581a41bafb75c7eeb5
chuccv <chuccv@mageplaza.com>
2022-02-21 17:33:42 +0700

    [Update Feature] GraphQL system.

---
+++ GraphQl/Model/Resolver/Configs/Get.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Gdpr
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\Gdpr\GraphQl\Model\Resolver\Configs;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\GraphQl\Model\Query\ContextInterface;
+use Mageplaza\Gdpr\Helper\Data;
+use Mageplaza\Gdpr\Model\Api\Data\Config\GeneralConfig;
+
+/**
+ * Class Get
+ * @package Mageplaza\Gdpr\GraphQl\Model\Resolver\Configs
+ */
+class Get implements ResolverInterface
+{
+    /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * @var GeneralConfig
+     */
+    protected $generalConfig;
+
+    /**
+     * Get constructor.
+     *
+     * @param Data $helperData
+     * @param GeneralConfig $generalConfig
+     */
+    public function __construct(
+        Data $helperData,
+        GeneralConfig $generalConfig
+    ) {
+        $this->helperData    = $helperData;
+        $this->generalConfig = $generalConfig;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $store   = $context->getExtensionAttributes()->getStore();
+        $storeId = $store->getId();
+
+        /** @var ContextInterface $context */
+        if (false === $context->getExtensionAttributes()->getIsCustomer()) {
+            throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+        }
+
+        if (!$this->helperData->isEnabled($storeId)) {
+            throw new GraphQlInputException(__('Gdpr is disabled.'));
+        }
+
+        $data = $this->getGeneralConfig($storeId);
+
+        return $this->generalConfig->addData($data);
+    }
+
+    /**
+     * @param $storeId
+     *
+     * @return array
+     */
+    public function getGeneralConfig($storeId)
+    {
+        return [
+            'general' => [
+                'enabled'                      => $this->helperData->getConfigGeneral('enabled', $storeId),
+                'allow_delete_customer'        => $this->helperData->getConfigGeneral('allow_delete_customer',
+                    $storeId),
+                'delete_customer_message'      => $this->helperData->getConfigGeneral('delete_customer_message',
+                    $storeId),
+                'allow_delete_default_address' => $this->helperData->getConfigGeneral('allow_delete_default_address',
+                    $storeId),
+            ]
+        ];
+    }
+}
