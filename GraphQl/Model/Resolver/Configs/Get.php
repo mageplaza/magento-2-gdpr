4fc6dbf4c954d9618c03ca2a1f30a7d7dc118fb8
chuccv <chuccv@mageplaza.com>
2022-02-24 13:12:55 +0700

    [Update Future] GraphQL Config.

--- GraphQl/Model/Resolver/Configs/Get.php
+++ GraphQl/Model/Resolver/Configs/Get.php
@@ -30,7 +30,6 @@ use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\GraphQl\Model\Query\ContextInterface;
 use Mageplaza\Gdpr\Helper\Data;
-use Mageplaza\Gdpr\Model\Api\Data\Config\GeneralConfig;
 
 /**
  * Class Get
@@ -44,20 +43,12 @@ class Get implements ResolverInterface
     protected $helperData;
 
     /**
-     * @var GeneralConfig
-     */
-    protected $generalConfig;
-
-    /**
      * @param Data $helperData
-     * @param GeneralConfig $generalConfig
      */
     public function __construct(
-        Data $helperData,
-        GeneralConfig $generalConfig
+        Data $helperData
     ) {
         $this->helperData    = $helperData;
-        $this->generalConfig = $generalConfig;
     }
 
     /**
@@ -77,9 +68,7 @@ class Get implements ResolverInterface
             throw new GraphQlInputException(__('Gdpr is disabled.'));
         }
 
-        $data = $this->getGeneralConfig($storeId);
-
-        return $this->generalConfig->addData($data);
+        return $this->getGeneralConfig($storeId);
     }
 
     /**
