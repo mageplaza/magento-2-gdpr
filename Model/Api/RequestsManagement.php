4ab6be41376bce14615fb0a7dc1d1971478f2628
chuccv <chuccv@mageplaza.com>
2022-02-24 14:56:02 +0700

    [Update Future] API deleteCustomerAccount & deleteDefaultAddress.

--- Model/Api/RequestsManagement.php
+++ Model/Api/RequestsManagement.php
@@ -21,8 +21,17 @@
 
 namespace Mageplaza\Gdpr\Model\Api;
 
+use Magento\Authorization\Model\UserContextInterface;
+use Magento\Customer\Api\AddressRepositoryInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\DataObject;
+use Magento\Framework\Event\ManagerInterface as EventManagerInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Registry;
+use Magento\Framework\Webapi\Exception as ExceptionApi;
 use Mageplaza\Gdpr\Api\RequestsManagementInterface;
+use Mageplaza\Gdpr\Helper\Data;
 use Mageplaza\Gdpr\Model\Api\Data\Config\GeneralConfig;
 
 /**
@@ -32,17 +41,64 @@ use Mageplaza\Gdpr\Model\Api\Data\Config\GeneralConfig;
 class RequestsManagement implements RequestsManagementInterface
 {
     /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $_customerRepository;
+
+    /**
+     * @var EventManagerInterface
+     */
+    protected $_eventManager;
+
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
      * @var GeneralConfig
      */
     protected $_generalConfig;
+    /**
+     * @var AddressRepositoryInterface
+     */
+    protected $_addressRepository;
+
+    /**
+     * @var UserContextInterface
+     */
+    protected $_userContext;
 
     /**
      * @param GeneralConfig $generalConfig
+     * @param AddressRepositoryInterface $addressRepository
+     * @param Data $helperData
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param EventManagerInterface $eventManager
+     * @param Registry $registry
+     * @param UserContextInterface $userContext
      */
     public function __construct(
-        GeneralConfig $generalConfig
+        GeneralConfig $generalConfig,
+        AddressRepositoryInterface $addressRepository,
+        Data $helperData,
+        CustomerRepositoryInterface $customerRepository,
+        EventManagerInterface $eventManager,
+        Registry $registry,
+        UserContextInterface $userContext
     ) {
-        $this->_generalConfig = $generalConfig;
+        $this->_generalConfig      = $generalConfig;
+        $this->_addressRepository  = $addressRepository;
+        $this->helperData          = $helperData;
+        $this->_customerRepository = $customerRepository;
+        $this->_eventManager       = $eventManager;
+        $this->registry            = $registry;
+        $this->_userContext        = $userContext;
     }
 
     /**
@@ -55,5 +111,73 @@ class RequestsManagement implements RequestsManagementInterface
                 'general_config' => $this->_generalConfig->getConfig()
             ]
         );
+    }
+
+    /**
+     * @inheritDoc
+     * @throws ExceptionApi
+     */
+    public function deleteDefaultAddress($addressId)
+    {
+        try {
+            $this->_addressRepository->deleteById($addressId);
+        } catch (\Exception $e) {
+            throw new ExceptionApi(__($e->getMessage()));
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws ExceptionApi
+     */
+    public function deleteCustomerAccount()
+    {
+        if (!$this->helperData->allowDeleteAccount()) {
+            $message = __('Not allow delete');
+            throw new ExceptionApi(__($message));
+        }
+
+        $checktoken = new DataObject(['flag' => true]);
+        $customerId = $this->getCurrentUserId();
+        try {
+            $customer = $this->_customerRepository->getById($customerId);
+        } catch (NoSuchEntityException $e) {
+            throw new ExceptionApi(__($e->getMessage()));
+        } catch (LocalizedException $e) {
+            throw new ExceptionApi(__($e->getMessage()));
+
+        }
+
+        /** event anonymise & delete customer before delete account */
+        $this->_eventManager->dispatch('anonymise_account_before_delete', compact('customer', 'checktoken'));
+
+        if (!$checktoken->getFlag()) {
+            $message = __('Flag not exit');
+
+            throw new ExceptionApi(__($message));
+        }
+        try {
+            $this->registry->register('isSecureArea', true, true);
+            /** When perform delete operation, magento check isSecureArea is true/false. */
+            $this->_customerRepository->deleteById($customerId);
+
+            /** event anonymise & delete customer after delete account */
+            $this->_eventManager->dispatch('anonymise_account_after_delete', ['customer' => $customer]);
+            $message = __('Customer id %1 has been deleted', $customerId);
+
+            return (string) $message;
+        } catch (\Exception $e) {
+            throw new ExceptionApi(__($e->getMessage()));
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentUserId()
+    {
+        return $this->_userContext->getUserId();
     }
 }
